77d78f644325fb27fd734aff11d376a00d829a12
赖巍 <skyoflw@gmail.com>
2016-04-21 01:38:15 +0800

    feat: simple to find window

---
+++ Jasily.Api.Windows/Win32/Window.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace Jasily.Api.Windows.Win32
+{
+    public static class Window
+    {
+        #region GetWindowText
+
+        /// <summary>
+        /// also see: https://msdn.microsoft.com/en-us/library/windows/desktop/ms633521(v=vs.85).aspx
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+
+        /// <summary>
+        /// alse see: https://msdn.microsoft.com/en-us/library/windows/desktop/ms633520(v=vs.85).aspx
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="lpString"></param>
+        /// <param name="nMaxCount"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        #endregion
+
+        #region EnumWindows
+
+        public delegate bool EnumWindowsProc(IntPtr hwnd, IntPtr lParam);
+
+        [DllImport("user32")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnumChildWindows(IntPtr window, EnumWindowsProc callback, IntPtr i);
+
+        #endregion
+
+        #region FindWindow
+
+        /// <summary>
+        /// Retrieves a handle to the top-level window whose class name and window name match the specified strings.
+        /// see: https://msdn.microsoft.com/en-us/library/windows/desktop/ms633499(v=vs.85).aspx
+        /// </summary>
+        /// <param name="lpClassName"></param>
+        /// <param name="lpWindowName"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow([CanBeNull] string lpClassName, [CanBeNull] string lpWindowName);
+
+        /// <summary>
+        /// see: https://msdn.microsoft.com/en-us/library/windows/desktop/ms633500(v=vs.85).aspx
+        /// </summary>
+        /// <param name="parentHandle"></param>
+        /// <param name="childAfter"></param>
+        /// <param name="className"></param>
+        /// <param name="windowTitle"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, [CanBeNull] string className, [CanBeNull] string windowTitle);
+
+        #endregion
+    }
+}
---
+++ Jasily.Api.Windows/Window.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace Jasily.Api.Windows
+{
+    public struct Window
+    {
+        public IntPtr Ptr { get; }
+
+        private Window(IntPtr ptr)
+        {
+            this.Ptr = ptr;
+        }
+
+        #region get window text
+
+        public string GetText()
+        {
+            if (this.Ptr == IntPtr.Zero) return null;
+            var length = Win32.Window.GetWindowTextLength(this.Ptr);
+            var sb = new StringBuilder(length + 1);
+            Win32.Window.GetWindowText(this.Ptr, sb, sb.Capacity);
+            return sb.ToString();
+        }
+
+        #endregion
+
+        #region get child
+
+        public Window? GetChild([CanBeNull] string className, [CanBeNull] string windowName)
+        {
+            if (this.Ptr == IntPtr.Zero) return null;
+            var ptr = Win32.Window.FindWindowEx(this.Ptr, IntPtr.Zero, className, windowName);
+            return WrapWindow(ptr);
+        }
+
+        public Window? GetChildAfter([CanBeNull] string className, [CanBeNull] string windowName, Window childAfter)
+        {
+            if (this.Ptr == IntPtr.Zero) return null;
+            var ptr = Win32.Window.FindWindowEx(this.Ptr, childAfter.Ptr, className, windowName);
+            return WrapWindow(ptr);
+        }
+
+        #endregion
+
+        #region get childs
+
+        public IEnumerable<Window> GetChilds()
+            => this.Ptr == IntPtr.Zero ? Enumerable.Empty<Window>() : WrapWindow(GetChildWindows(this.Ptr));
+
+        private static IEnumerable<IntPtr> GetChildWindows(IntPtr parent)
+        {
+            var result = new List<IntPtr>();
+            var listHandle = GCHandle.Alloc(result);
+            try
+            {
+                Win32.Window.EnumChildWindows(parent, EnumWindow, GCHandle.ToIntPtr(listHandle));
+            }
+            finally
+            {
+                if (listHandle.IsAllocated) listHandle.Free();
+            }
+            return result;
+        }
+
+        private static bool EnumWindow(IntPtr handle, IntPtr pointer)
+        {
+            var gch = GCHandle.FromIntPtr(pointer);
+            var list = gch.Target as List<IntPtr>;
+            if (list == null) throw new InvalidCastException();
+            list.Add(handle);
+            return true;
+        }
+
+        #endregion
+
+        private static IEnumerable<Window> WrapWindow(IEnumerable<IntPtr> ptrs)
+            => from ptr in ptrs where ptr != IntPtr.Zero select new Window(ptr);
+
+        private static Window? WrapWindow(IntPtr ptr) => ptr == IntPtr.Zero ? (Window?)null : new Window(ptr);
+
+        public static Window? Find([CanBeNull] string className, [CanBeNull] string windowName)
+            => WrapWindow(Win32.Window.FindWindow(className, windowName));
+
+        public static Window? DeepFindByClassNames([NotNull] params string[] classNames)
+        {
+            if (classNames == null) throw new ArgumentNullException(nameof(classNames));
+            if (classNames.Length == 0) return null;
+            var ptr = IntPtr.Zero;
+            for (var i = 0; i < classNames.Length; i++)
+            {
+                ptr = Win32.Window.FindWindowEx(ptr, IntPtr.Zero, classNames[i], null);
+                if (ptr == IntPtr.Zero) return null;
+            }
+            return WrapWindow(ptr);
+        }
+
+        public static Window? DeepFindByQueryParameter([NotNull] params WindowQueryParameter[] query)
+        {
+            if (query == null) throw new ArgumentNullException(nameof(query));
+            if (query.Length == 0) return null;
+            var ptr = IntPtr.Zero;
+            for (var i = 0; i < query.Length; i++)
+            {
+                Win32.Window.FindWindowEx(ptr, IntPtr.Zero, query[i].ClassName, query[i].WindowName);
+                if (ptr == IntPtr.Zero) return null;
+            }
+            return WrapWindow(ptr);
+        }
+
+        public struct WindowQueryParameter
+        {
+            [CanBeNull]
+            public string ClassName { get; set; }
+
+            [CanBeNull]
+            public string WindowName { get; set; }
+        }
+    }
+}
